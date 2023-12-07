726e2c9998f2fa25e776f39a597b72e7125e9174
Panda <pauldalapatt@gmail.com>
2023-12-07 22:37:37 +0530

    Added error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
-import userRoutes from "./routes/userRoutes.js"
+import userRoutes from "./routes/userRoutes.js";
+import { noEndpoint, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
-
 
 app.use("/api/user", userRoutes);
 if (process.env.DEV === "true") {
   app.get("/", (req, res) => res.send("Api home"));
 } else {
-    app.get("/",(req,res)=>res.status(401).send("Unauthorized"))
+  app.get("/", (req, res) => res.status(401).send("Unauthorized"));
 }
-
+app.use(noEndpoint);
+app.use(errorHandler);
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
