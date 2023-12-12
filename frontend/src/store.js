3fccafb5094860a7d553726435da45e15bcaaa92
Panda <pauldalapatt@gmail.com>
2023-12-12 15:35:36 +0530

    Minor changes

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
-import publicReducer from "./slices/publicSlice"
+import publicReducer from "./slices/publicSlice";
 const store = configureStore({
   reducer: {
-    public:publicReducer,
+    public: publicReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({serializableCheck:false}).concat(apiSlice.middleware),
-  devTools: process.env.Dev==="true",
+    getDefaultMiddleware({ serializableCheck: false }).concat(
+      apiSlice.middleware
+    ),
+  devTools: process.env.Dev === "true",
 });
 
 export default store;
